54e53ad43e727a6b95cc28aaa40c8140c2843938
Laurence Luo <admin@lzc.app>
2024-05-30 23:32:04 +0800

    Update

--- ISC.py
+++ ISC.py
@@ -8,17 +8,17 @@ headers = {'Connection': 'Keep-Alive',
            'Accept-Language': 'en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3',
            'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
 
-for year in range(1945, 2024):
-    for num in range(1, 51):
+for year in range(1945, 2025):
+    for num in range(1, 50):
         try:
             number = str(year) + str(num).zfill(2)
-            url = 'http://data.istrongcloud.com/v2/data/complex/' + number + '.json'
+            url = 'https://data.istrongcloud.com/v2/data/complex/' + number + '.json'
             req = urllib.request.Request(url=url, headers=headers)
             data = urllib.request.urlopen(req).read()
             data = json.loads(data)[0]
 
             df = pd.json_normalize(data['points'])
-            df.to_csv('./typhoon/istrongcloud/' + number + '.csv', encoding='utf_8_sig', index=False)
+            df.to_csv('./typhoon/ISC/' + number + '.csv', encoding='utf_8_sig', index=False)
 
             print('Downloaded: ' + number)
         except:
--- NII.py
+++ NII.py
@@ -1,23 +1,64 @@
 import re
 
-import pandas
 import requests
-from lxml import etree
 
-url = "http://agora.ex.nii.ac.jp/cgi-bin/dt/search_name2.pl?lang=en&basin=wnp&smp=1&sdp=1&emp=12&edp=31"
-response = requests.get(url)
-response.encoding = 'utf8'
-html = response.text
-d = re.findall(r'<td><a href="/digital-typhoon/summary/wnp/s/(.*?)">', html, re.S)
+nii_url = "https://agora.ex.nii.ac.jp/cgi-bin/dt/search_name2.pl?lang=en&basin=wnp&smp=1&sdp=1&emp=12&edp=31"
+nii_response = requests.get(nii_url)
+nii_response.encoding = 'utf8'
+nii_html = nii_response.text
+d = re.findall(r'<a href="/digital-typhoon/summary/wnp/s/(.*?)">', nii_html, re.S)
+
+
+def save_kml(tid):
+    url = 'https://agora.ex.nii.ac.jp/digital-typhoon/kml/wnp/' + tid + '.en.kml'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        with open('./typhoon/NII/GoogleEarth/' + tid + '.kml', 'wb') as f:
+            f.write(response.content)
+
+
+def save_tkml(tid):
+    url = 'https://agora.ex.nii.ac.jp/digital-typhoon/kml/wnp/' + tid + '-t.en.kml'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        with open('./typhoon/NII/GEAnimation/' + tid + '-t.kml', 'wb') as f:
+            f.write(response.content)
+
+
+def save_atom(tid):
+    url = 'https://agora.ex.nii.ac.jp/digital-typhoon/atom/typhoon/wnp/' + tid + '.en.atom'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        with open('./typhoon/NII/AtomFeed/' + tid + '.atom', 'wb') as f:
+            f.write(response.content)
+
+
+def save_geojson(tid):
+    url = 'https://agora.ex.nii.ac.jp/digital-typhoon/geojson/wnp/' + tid + '.en.json'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        geojson_data = response.json()
+
+        with open('./typhoon/NII/GeoJSON/' + tid + '.geojson', 'wb') as f:
+            f.write(response.content)
+
 
 for p in d:
     tid = p[:6]
-    if int(tid) >= 197701:
-        p_url = 'http://agora.ex.nii.ac.jp/digital-typhoon/summary/wnp/k/' + p
-        response_1 = requests.get(p_url)
-        html_1 = etree.HTML(response_1.text)
-        table = html_1.xpath('//table[@class="TRACKINFO"]')
-        table = etree.tostring(table[0], encoding='utf-8').decode()
-        df = pandas.read_html(table, encoding='utf-8', header=0)[0]
-        df.to_csv('./typhoon/nii/' + tid + ".csv", mode='w', encoding='utf_8_sig', index=False)
-        print('Downloaded: ' + tid)
+    if int(tid) >= 195101:
+        try:
+            # p_url = 'http://agora.ex.nii.ac.jp/digital-typhoon/summary/wnp/k/' + p
+
+            save_kml(tid)
+            save_tkml(tid)
+            save_atom(tid)
+            save_geojson(tid)
+
+            print('Downloaded: ' + tid)
+
+        except:
+            print("Error: " + str(tid))
